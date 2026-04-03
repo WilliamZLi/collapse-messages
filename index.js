deb3e832a1b089101cbf4f82700b9028d1518a30
Ivari <11774-Ivari@users.noreply.gitgud.io>
2026-04-02 23:21:14 -0700

    custom

--- index.js
+++ index.js
@@ -1,63 +1,146 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
+import { extension_settings, getContext } from "../../../extensions.js";
+import { saveSettingsDebounced, eventSource, event_types } from "../../../../script.js";
 
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
-import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
-
-//You'll likely need to import some other functions from the main script
-import { saveSettingsDebounced } from "../../../../script.js";
-
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+const extensionName = "collapse-messages";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const extensionSettings = extension_settings[extensionName];
-const defaultSettings = {};
 
+const defaultSettings = {
+    collapsedLines: 3,
+    collapsed: {},
+};
 
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
-async function loadSettings() {
-  //Create the settings if they don't exist
-  extension_settings[extensionName] = extension_settings[extensionName] || {};
-  if (Object.keys(extension_settings[extensionName]).length === 0) {
-    Object.assign(extension_settings[extensionName], defaultSettings);
-  }
-
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+function getSettings() {
+    return extension_settings[extensionName];
 }
 
-// This function is called when the extension settings are changed in the UI
-function onExampleInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].example_setting = value;
-  saveSettingsDebounced();
+function getChatId() {
+    return getContext().chatId || null;
 }
 
-// This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
+function isCollapsed(mesId) {
+    const chatId = getChatId();
+    if (!chatId) return false;
+    return (getSettings().collapsed[chatId] || []).includes(mesId);
 }
 
-// This function is called when the extension is loaded
+function setCollapsed(mesId, collapsed) {
+    const chatId = getChatId();
+    if (!chatId) return;
+    const settings = getSettings();
+    if (!settings.collapsed[chatId]) {
+        settings.collapsed[chatId] = [];
+    }
+    const arr = settings.collapsed[chatId];
+    const idx = arr.indexOf(mesId);
+    if (collapsed && idx === -1) {
+        arr.push(mesId);
+    } else if (!collapsed && idx !== -1) {
+        arr.splice(idx, 1);
+    }
+    saveSettingsDebounced();
+}
+
+function applyCollapseVisual(mesElement, collapsed) {
+    const mesText = mesElement.find(".mes_text");
+    const btn = mesElement.find(".mes_collapse_btn");
+    if (collapsed) {
+        mesText.addClass("mes_text_collapsed");
+        btn.attr("title", "Expand message")
+            .removeClass("fa-compress")
+            .addClass("fa-expand");
+    } else {
+        mesText.removeClass("mes_text_collapsed");
+        btn.attr("title", "Collapse message")
+            .removeClass("fa-expand")
+            .addClass("fa-compress");
+    }
+}
+
+function toggleCollapse(mesElement) {
+    const mesId = parseInt(mesElement.attr("mesid"));
+    const nowCollapsed = !isCollapsed(mesId);
+    setCollapsed(mesId, nowCollapsed);
+    applyCollapseVisual(mesElement, nowCollapsed);
+}
+
+function addCollapseButton(mesElement) {
+    if (mesElement.find(".mes_collapse_btn").length > 0) return;
+    const btn = $(
+        `<div class="mes_button mes_collapse_btn fa-solid fa-compress" title="Collapse message"></div>`
+    );
+    btn.on("click", function (e) {
+        e.stopPropagation();
+        toggleCollapse(mesElement);
+    });
+    mesElement.find(".mes_buttons").prepend(btn);
+}
+
+function initMessage(mesElement) {
+    if (!mesElement.length) return;
+    addCollapseButton(mesElement);
+    const mesId = parseInt(mesElement.attr("mesid"));
+    if (isCollapsed(mesId)) {
+        applyCollapseVisual(mesElement, true);
+    }
+}
+
+function initAllMessages() {
+    $("#chat .mes").each(function () {
+        initMessage($(this));
+    });
+}
+
+function updateLineClampVar() {
+    const lines = getSettings().collapsedLines || 3;
+    document.documentElement.style.setProperty("--collapse-lines", lines);
+}
+
+function loadSettings() {
+    extension_settings[extensionName] = extension_settings[extensionName] || {};
+    if (Object.keys(extension_settings[extensionName]).length === 0) {
+        Object.assign(extension_settings[extensionName], defaultSettings);
+    }
+    if (!extension_settings[extensionName].collapsed) {
+        extension_settings[extensionName].collapsed = {};
+    }
+    updateLineClampVar();
+    $("#collapse_lines_input").val(getSettings().collapsedLines);
+}
+
 jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+    const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
+    $("#extensions_settings2").append(settingsHtml);
 
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
-  $("#extensions_settings").append(settingsHtml);
+    loadSettings();
+    initAllMessages();
 
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
+    eventSource.on(event_types.CHARACTER_MESSAGE_RENDERED, (message_id) => {
+        initMessage($(`#chat .mes[mesid="${message_id}"]`));
+    });
 
-  // Load settings when starting things up (if you have any)
-  loadSettings();
+    eventSource.on(event_types.USER_MESSAGE_RENDERED, (message_id) => {
+        initMessage($(`#chat .mes[mesid="${message_id}"]`));
+    });
+
+    // Re-apply collapse after a message is edited (mes_text content is replaced)
+    eventSource.on(event_types.MESSAGE_EDITED, (message_id) => {
+        const mesElement = $(`#chat .mes[mesid="${message_id}"]`);
+        if (mesElement.length && isCollapsed(parseInt(message_id))) {
+            applyCollapseVisual(mesElement, true);
+        }
+    });
+
+    eventSource.on(event_types.CHAT_CHANGED, () => {
+        // Give ST time to render the new chat's messages
+        setTimeout(initAllMessages, 300);
+    });
+
+    $("#collapse_lines_input").on("input", function () {
+        const val = parseInt($(this).val());
+        if (val >= 1) {
+            getSettings().collapsedLines = val;
+            saveSettingsDebounced();
+            updateLineClampVar();
+        }
+    });
 });
